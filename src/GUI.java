b872ff7bdad307432ef88021d95d8c05fee6ae9b
flyai <flynnai@umich.edu>
2023-04-19 08:13:25 +0200

    Disable window resizing

--- src/GUI.java
+++ src/GUI.java
@@ -5,7 +5,8 @@ public class GUI extends Application implements GameConstants {
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Jaconde Test");
-
+        primaryStage.setResizable(false);
+        
         // initiate GamePage instance and show in stage
         GamePage gamePage = new GamePage();
         primaryStage.setScene(gamePage.getScene());
